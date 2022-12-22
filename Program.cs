83cde868aa3c779ccc72634ee958e3f95c9f1d18
Thomas <vo.thomas.11647@outlook.com>
2022-12-22 15:13:37 +1100

    Calculator Library

--- Program.cs
+++ Program.cs
@@ -1,38 +1,8 @@
 ﻿using System;
-namespace Calculator
+using CalculatorLibrary;
+using System.Diagnostics;
+namespace CalculatorProgram
 {
-    class Calculator
-    {
-        public static double DoOperation(double num1, double num2, string op)
-        {
-            double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-            // Use a switch statement to do the math.
-            switch (op)
-            {
-                case "a":
-                    result = num1 + num2;
-                    break;
-                case "s":
-                    result = num1 - num2;
-                    break;
-                case "m":
-                    result = num1 * num2;
-                    break;
-                case "d":
-                    // Ask the user to enter a non-zero divisor.
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    break;
-                // Return text for an incorrect option entry.
-                default:
-                    break;
-            }
-            return result;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
@@ -41,6 +11,7 @@ namespace Calculator
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
+
 
             while (!endApp)
             {
